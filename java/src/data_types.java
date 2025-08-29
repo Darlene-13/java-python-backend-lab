cc40c94e49aa82e7e69360054e6e084952101436
Darlene-13 <darlenenasimiyu@gmail.com>
2025-08-30 02:40:27 +0300

    Java datatypes and arrays

---
+++ java/src/data_types.java
@@ -0,0 +1,28 @@
+// This are some basic data types in java
+public class data_types {
+    public static void main(String[] args){
+
+        // Primitive data types
+        byte b = 5; // 1 byte
+        int numbers = 10; // 4 bytes
+        boolean isgood = true; // 1 byte
+        char c = 'A'; // 2 bytes
+        float f = 5.99f; // 4 bytes
+        double d = 19.99; // 8 bytes
+        System.out.println("Float value: " + f);
+        System.out.println("Double value: " + d);
+
+        // Objects
+        String filename = "document.txt";
+        byte[] dataArray = new byte[1024]; // 1 KB array
+
+
+        // Creating new arrays
+        byte[] fileData = new byte[1000];
+        int[] numbers = new int[10]
+
+        // Working with arrays
+        fileData[0] = 1;
+        numbers[0] = 10;
+    }
+}
