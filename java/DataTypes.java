e8920545eee757a4f10982a3756e63185f41b8bc
Darlene-13 <darlenenasimiyu@gmail.com>
2025-09-02 21:26:23 +0300

    Boolean datatype

---
+++ java/DataTypes.java
@@ -0,0 +1,21 @@
+// Java has two main data types: Primitive and Non-Primitive.
+// Primitive data types can be categorized into two: Numeric and Non-numeric
+// Numeric primitive data types are: Bytes, Long, Short, Int, Float, Double
+// Non-numeric data types are: Boolean and Char
+// Non primitive data types are: String, Arrays, Classes, Interface, Objects, Interface
+
+
+//PRIMITIVE DATA TYPES: Boolean
+// This is a data type that represents a logical value that can either be true or false. It is VM dependent and it only occupies 1 byte of data (8 bits)
+public class DataTypes {
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2 = false;
+
+        System.out.println("Is Java fun?" + b1);
+        System.out.println("Is Java boring?" + b2);
+    }
+}
+
+// PRIMITIVE DATA TYPES: Char
+// This is a data type that represents a single 18 bit Unicode character, that is 2 bytes
