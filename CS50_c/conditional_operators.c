422e9bdbec468a58220879bad3a1b1fbeed00983
Darlene-13 <darlenenasimiyu@gmail.com>
2025-09-09 11:21:57 +0300

    Conditional operators in c

---
+++ CS50_c/conditional_operators.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (void) {
+
+    int score = 95;
+    printf("What was your test score? ");
+    scanf("%d", &score);
+
+    // Comparison operators with conditionals
+    if ( score < 60 ){
+        printf("You failed the test.\n");
+    }
+    else if (score > 80 ){
+        printf("You passed with distinction.\n");
+    }
+    else {
+        printf("You passed the test.\n");
+    }
+    return 0;
+}
