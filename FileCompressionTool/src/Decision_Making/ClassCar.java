437fc6aefe47d022e9edece6c6974b6f5b24adf0
Darlene-13 <darlenenasimiyu@gmail.com>
2025-10-03 16:22:42 +0300

    Updated on looping and decision making

---
+++ FileCompressionTool/src/Decision_Making/ClassCar.java
@@ -0,0 +1,21 @@
+
+
+public class ClassCar{
+
+    String model; // state(field)
+    int speed; // State(field)
+
+    ClassCar(String model, int speed){
+        this.model = model;
+        this.speed = speed;
+
+
+        // Behaviour or method
+        void drive(){
+            System.out.println()(model + " is driving at " + speed + "km/h" );
+        }
+    }
+    Public static void main(String[] args){
+
+    }
+}
---
+++ FileCompressionTool/src/Decision_Making/JavaArray.java
@@ -0,0 +1,26 @@
+
+
+
+
+public class JavaArray{
+    public static void main(String[] args){
+        // Initializing an array
+        int [] arr = {40,55,63,17,22};
+
+        // Size of array
+        int n = arr.length;
+
+        // traversing an array
+        for (int i=0; i < n; i++){
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
+
+// Key features of arrays
+/*
+* Store primitives and objects (Can hold both primitive dat types like int, char and boolean amongst others) and objects like (String, Integer etcs)
+* */
+
+// An object in java is a real world entity represented in the code. It is an instance of a class.
+// A class is a blueprint and an object is a living thing built from the blue print.
