7bf1ceae7bf100198c51413eba2ce86041cfcf4d
Darlene-13 <darlenenasimiyu@gmail.com>
2025-10-04 12:43:53 +0300

    Instance and static methods

--- FileCompressionTool/src/JavaMethods/StaticInstance.java
+++ FileCompressionTool/src/JavaMethods/StaticInstance.java
@@ -25,4 +25,27 @@ class StaticInstance{
 // Static methods are called without creating an object.
 
 // Instance method: They require an object to work, we need to create an object of the class where the method is written.
-// Memory allocation:
+// Memory allocation: They are stored in the permanent generation space of the heap for better efficiency and their parameters that is local variables and return values are allocated on the stack.
+// They belomg to the object and not to the class thus they require an object to be called, they are stored in one memoery location and they can be overriden as they use dynamic binding runtime.
+
+
+class StaticInstance2 {
+    String n = "";
+
+    // Instance method that takes a String
+    void test(String value){
+        this.n = value;  // assign argument to instance variable
+    }
+}
+
+class StaticInstance3 {
+    public static void main(String[] args){
+        // Create an instance of the class
+        StaticInstance2 obj = new StaticInstance2();
+
+        // Call the instance method
+        obj.test("Test your API!");
+        System.out.println(obj.n);  // prints: Test your API!
+    }
+}
+
