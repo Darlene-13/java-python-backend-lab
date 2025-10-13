cd7a6eb732b0139bf1f8a36308aedaefb700bbe9
Darlene-13 <darlenenasimiyu@gmail.com>
2025-10-13 17:21:16 +0300

    Introduction to error and exception handling

--- FileCompressionTool/src/ExceptionHandling/JavaException.java
+++ FileCompressionTool/src/ExceptionHandling/JavaException.java
@@ -53,4 +53,90 @@ class ThrowError (
 }
 )
 
-// b. Throws
+// b. Throws: This declares the exception that a method might throw, thus informing the caller to handle them. It is mainly used with checked exceptions as below. If a method calls another method that thros a checked exception and it does not catch it thus it must declare that exception in its throws case
+
+
+
+import java.io.*;
+
+class ThrowsException {
+    static void readFIle(String fileName) throws IOException{
+        FileReader = new FileReader(fileName);
+    }
+    public static void main(String[] args){
+        try{
+            readFile("test.txt");
+        } catch(IOException e){
+            System.out.println("File not found: " e.getMessage());
+        }
+    }
+}
+
+// Internal working of a try catch block
+/*
+* JVM executes the code inside the try block
+* if an exception occurs, the remaining try code is skipped and JVM search for matching catch block
+* If catch block found the block executes
+* Controls then moves to the finally block
+* If not matching catch is found the exception is handled by JVM default handler
+* The finally block always executes whether the exception occurs or not.
+* */
+
+
+// All exceptions in java and errors are subclasses oft the throwable class which has two branches the exception and the error class.
+
+// TYPES OF JAVA EXCEPTION
+//1. User defined exceptions
+//2. Built in exceptions which can grouped to checked and unchecked.
+// a. Checked exceptions are like: ClassNotFound, InterruptedException, IOException, InstantiationException, SQLException, FileNotFoundException.
+// b. Unchecked Exceptions are like: ArithmeticException, ClassCastException, NullPointerException, ArrayOutOfIndexException, ArrayStoreException, IllegalThreadStateException
+
+
+//1. Builtin exceptions: Built in exceptiions are pre-defined exception classes provided by java to handle common errors during the execution of a program.
+// There are two types of built in exceptions in java the checked and unchecked exceptions, checked are checked at compile time while the unchecked are checked at runtime
+
+// 2. User-defined exceptions: Sometimes built in exceptions won't full describe a certain situation and in such cases users can create exceptions.
+//METHODS TO PRINT EXCEPTION INFORMATION
+//1. printStactTrace() : This prints the full stack trace of the exception including the name of the messaage and the error location.
+// 2. toString()" This prints the exception information in the format of the name of the exception
+//3. getMessage(): Prints the description of the exception.
+
+
+
+// NESTED TRY CATCH: In java the try-catch block can be nested inside another to handle exceptions at multiple levels
+
+class NestedTryCatchExample{
+    public static void main(String[] args){
+        try{
+            System.out.println("Outer try block");
+        } catch( ArithmeticException e){
+            System.out.println("Inner catch" + e);
+        }
+        String str = null;
+        System.out.println(str.length()); // This causes the null pointer exception
+    } catch(NullPointerExeption e){
+        System.out.println("Outer catch: " + e);
+    }
+}
+
+// Handling multiple exceptions
+// We can handle multiple exceptions in java by using multiple try catch blocks each of the catching a different type of exception.
+
+//HOW JVM HANDLES AN EXCEPTION
+/*
+* When an exception occurs, JVM  creates an exception object that contains the error name , the decription and the program state.
+* Creating the exception object and handling it in the run time system is called throwing an exception.
+* There however might be alist of methods that had been called to get to the method where the exception occured.
+* This ordered list of methods is called stack and the following proceedure will happen.
+*
+* 1. The run-time system searches the call stack for an exception handler
+* 2. It starts searching from the method where the exception occured and proceeds backwards through the call stack.
+* 3. If a handler is found the exception is passed to it.
+* 4. If not handler is found, the default exception handker terminates the program and prints the stack trace.
+* */
+
+
+
+//NOTE: OUTSIDE EXCEPTION WHEN WE ARE MAKING QUEUES, WE USE PRIORITY OR A LINKED LIST SINCE WE DON'T HAVE JUST  QUEUE
+// Queue: Queue<String> queue = new LinkedList<String>();
+// Queue: Queue<String> queue = new PriorityQueue<String>();
